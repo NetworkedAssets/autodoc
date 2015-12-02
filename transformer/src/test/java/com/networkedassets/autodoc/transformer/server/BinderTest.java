baa1c3a9135d3b1d1fa0c1b4734af1829a4ef79a
PGWIZDALSKI <PGWIZDALSKI@PC-GWIZDALSKI>
2015-12-02 22:35:56 +0100

    Change import to org.junit.Assert in test classes

--- transformer/src/test/java/com/networkedassets/autodoc/transformer/server/BinderTest.java
+++ transformer/src/test/java/com/networkedassets/autodoc/transformer/server/BinderTest.java
@@ -16,89 +16,92 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Testing dependency injection
  */
 public class BinderTest extends JerseyTest {
 
-    @Test
-    public void invokeGitCodeProvider() {
-        invoke(GitCodeProvider.class);
-    }
+	@Test
+	public void invokeGitCodeProvider() {
+		invoke(GitCodeProvider.class);
+	}
 
-    @Test
-    public void invokeSettingsManager() {
-        invoke(SettingsManager.class);
-    }
+	@Test
+	public void invokeSettingsManager() {
+		invoke(SettingsManager.class);
+	}
 
-    @Test
-    public void invokeDefaultDocumentationGeneratorFactory() {
-        invoke(DefaultDocumentationGeneratorFactory.class);
-    }
+	@Test
+	public void invokeDefaultDocumentationGeneratorFactory() {
+		invoke(DefaultDocumentationGeneratorFactory.class);
+	}
 
-    @Test
-    public void invokeDocumentationFromCodeGenerator() {
-        invoke(DocumentationFromCodeGenerator.class);
-    }
+	@Test
+	public void invokeDocumentationFromCodeGenerator() {
+		invoke(DocumentationFromCodeGenerator.class);
+	}
 
-    /**
-     * invokes RESTful resource to obtain class name injected in Resource class and compare its equality
-     * @param codeProvider
-     */
-    private void invoke(Class codeProvider) {
-        final String codeProviderName = codeProvider.getName();
-        Response response = target("example/" + codeProviderName).request().get();
-        assertEquals(codeProviderName, response.readEntity(String.class));
-    }
+	/**
+	 * invokes RESTful resource to obtain class name injected in Resource class
+	 * and compare its equality
+	 * 
+	 * @param codeProvider
+	 */
+	private void invoke(Class<?> codeProvider) {
+		final String codeProviderName = codeProvider.getName();
+		Response response = target("example/" + codeProviderName).request().get();
+		assertEquals(codeProviderName, response.readEntity(String.class));
+	}
 
-    @Override
-    protected Application configure(){
-        return new CustomResourceConfig();
-    }
+	@Override
+	protected Application configure() {
+		return new CustomResourceConfig();
+	}
 
-    private class CustomResourceConfig extends ResourceConfig {
-        public CustomResourceConfig() {
-            register(new Binder());
-            register(Resource.class);
-        }
-    }
+	private class CustomResourceConfig extends ResourceConfig {
+		public CustomResourceConfig() {
+			register(new Binder());
+			register(Resource.class);
+		}
+	}
 
-    /**
-     * RESTful resource using to @Inject different classes and compare class name passed in URI to injected class name
-     */
-    @Path("/example")
-    public static class Resource {
-        @Inject
-        private CodeProvider codeProvider;
-        @Inject
-        private SettingsManager settingsManager;
-        @Inject
-        private DefaultDocumentationGeneratorFactory defaultDocumentationGeneratorFactory;
-        @Inject
-        private DocumentationFromCodeGenerator documentationFromCodeGenerator;
+	/**
+	 * RESTful resource using to @Inject different classes and compare class
+	 * name passed in URI to injected class name
+	 */
+	@Path("/example")
+	public static class Resource {
+		@Inject
+		private CodeProvider codeProvider;
+		@Inject
+		private SettingsManager settingsManager;
+		@Inject
+		private DefaultDocumentationGeneratorFactory defaultDocumentationGeneratorFactory;
+		@Inject
+		private DocumentationFromCodeGenerator documentationFromCodeGenerator;
 
-        @GET
-        @Path("/{serviceClass}")
-        public Response getDynamicInvokedService(@PathParam("serviceClass") String serviceClass) {
-            String injectedServiceClassName = "";
+		@GET
+		@Path("/{serviceClass}")
+		public Response getDynamicInvokedService(@PathParam("serviceClass") String serviceClass) {
+			String injectedServiceClassName = "";
 
-            switch(serviceClass){
-                case "com.networkedassets.autodoc.transformer.handleRepoPush.infrastructure.GitCodeProvider":
-                    injectedServiceClassName = codeProvider.getClass().getName();
-                    break;
-                case "com.networkedassets.autodoc.transformer.manageSettings.core.SettingsManager":
-                    injectedServiceClassName = settingsManager.getClass().getName();
-                    break;
-                case "com.networkedassets.autodoc.transformer.handleRepoPush.core.DefaultDocumentationGeneratorFactory":
-                    injectedServiceClassName = defaultDocumentationGeneratorFactory.getClass().getName();
-                    break;
-                case "com.networkedassets.autodoc.transformer.handleRepoPush.core.DocumentationFromCodeGenerator":
-                    injectedServiceClassName = documentationFromCodeGenerator.getClass().getName();
-                    break;
-            }
-            return Response.ok(injectedServiceClassName).build();
-        }
-    }
+			switch (serviceClass) {
+			case "com.networkedassets.autodoc.transformer.handleRepoPush.infrastructure.GitCodeProvider":
+				injectedServiceClassName = codeProvider.getClass().getName();
+				break;
+			case "com.networkedassets.autodoc.transformer.manageSettings.core.SettingsManager":
+				injectedServiceClassName = settingsManager.getClass().getName();
+				break;
+			case "com.networkedassets.autodoc.transformer.handleRepoPush.core.DefaultDocumentationGeneratorFactory":
+				injectedServiceClassName = defaultDocumentationGeneratorFactory.getClass().getName();
+				break;
+			case "com.networkedassets.autodoc.transformer.handleRepoPush.core.DocumentationFromCodeGenerator":
+				injectedServiceClassName = documentationFromCodeGenerator.getClass().getName();
+				break;
+			}
+			return Response.ok(injectedServiceClassName).build();
+		}
+	}
 }
--- transformer/src/test/java/com/networkedassets/autodoc/transformer/util/PropertyHandlerTest.java
+++ transformer/src/test/java/com/networkedassets/autodoc/transformer/util/PropertyHandlerTest.java
@@ -1,26 +1,27 @@
 package com.networkedassets.autodoc.transformer.util;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
+import org.junit.Test;
 
 public class PropertyHandlerTest {
-    @Test
-    public void testGetValue() throws IOException {
-        PropertyHandler propertyHandler = PropertyHandler.getInstance();
-        assertNotNull(propertyHandler);
+	@Test
+	public void testGetValue() throws IOException {
+		PropertyHandler propertyHandler = PropertyHandler.getInstance();
+		assertNotNull(propertyHandler);
 
-        assertNotNull(propertyHandler.getValue("jetty.port", "8050"));
-        assertEquals(propertyHandler.getValue("jetty.port", "def"), "8050");
-        assertEquals(propertyHandler.getValue("not-found-key", "default-value"), "default-value");
+		assertNotNull(propertyHandler.getValue("jetty.port", "8050"));
+		assertEquals(propertyHandler.getValue("jetty.port", "def"), "8050");
+		assertEquals(propertyHandler.getValue("not-found-key", "default-value"), "default-value");
 
-        assertEquals(propertyHandler.getValue("settings.filename", "default-value"), "transformerSettings.ser");
-        assertEquals(propertyHandler.getValue("doclet.filename", "default-value"), "json-doclet-1.0.7.jar");
-        assertEquals(propertyHandler.getValue("doclet.classname", "default-value"), "com.github.markusbernhardt.xmldoclet.XmlDoclet");
-        assertEquals(propertyHandler.getValue("doclet.outfilename", "default-value"), "javadoc.json");
+		assertEquals(propertyHandler.getValue("settings.filename", "default-value"), "transformerSettings.ser");
+		assertEquals(propertyHandler.getValue("doclet.filename", "default-value"), "json-doclet-1.0.7.jar");
+		assertEquals(propertyHandler.getValue("doclet.classname", "default-value"),
+				"com.github.markusbernhardt.xmldoclet.XmlDoclet");
+		assertEquals(propertyHandler.getValue("doclet.outfilename", "default-value"), "javadoc.json");
 
-    }
+	}
 }
