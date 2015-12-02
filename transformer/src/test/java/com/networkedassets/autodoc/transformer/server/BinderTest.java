b5f0dd617d5af0fc48bfb51c468e84114abf0f4c
Maciej Tułaza <mtulaza@networkedassets.net>
2015-12-02 13:43:06 +0100

    jersey-test dependencies added; BinderTest class created (testing GitCodeProvider only)

--- transformer/src/test/java/com/networkedassets/autodoc/transformer/server/BinderTest.java
+++ transformer/src/test/java/com/networkedassets/autodoc/transformer/server/BinderTest.java
@@ -1,4 +1,54 @@
 package com.networkedassets.autodoc.transformer.server;
 
-public class BinderTest {
+import com.networkedassets.autodoc.transformer.handleRepoPush.infrastructure.GitCodeProvider;
+import com.networkedassets.autodoc.transformer.handleRepoPush.require.CodeProvider;
+import junit.framework.Assert;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.*;
+import javax.ws.rs.core.Application;
+
+import static junit.framework.Assert.*;
+
+public class BinderTest extends JerseyTest {
+
+    @Test
+    public void invokeGitCodeProvider(){
+        invoke(GitCodeProvider.class);
+    }
+
+    private <T extends CodeProvider> void invoke(Class<T> codeProvider) {
+        final String codeProviderName = codeProvider.getName();
+        Response response = target("example/" + codeProviderName).request().get();
+        assertEquals(codeProviderName, response.readEntity(String.class));
+    }
+
+    @Override
+    protected Application configure(){
+        return new CustomResourceConfig();
+    }
+
+    private class CustomResourceConfig extends ResourceConfig {
+        public CustomResourceConfig() {
+            register(new Binder());
+            register(Resource.class);
+        }
+    }
+
+    @Path("/example")
+    public static class Resource {
+        @Inject
+        private CodeProvider codeProvider;
+
+        @GET
+        @Path("/{serviceClass}")
+        public Response getDynamicInvokedService() {
+            return Response.ok(codeProvider.getClass().getName()).build();
+        }
+    }
 }
