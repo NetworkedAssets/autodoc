64eef93b7c0a2fafdf7e33ff3519fc457899ea82
Mariusz Gilewicz <mgilewicz@networkedassets.org>
2015-12-02 16:31:35 +0100

    Tests for PropertyHandler and PlantUML

--- transformer/src/test/java/com/networkedassets/autodoc/transformer/util/PropertyHandlerTest.java
+++ transformer/src/test/java/com/networkedassets/autodoc/transformer/util/PropertyHandlerTest.java
@@ -14,8 +14,13 @@ public class PropertyHandlerTest {
         assertNotNull(propertyHandler);
 
         assertNotNull(propertyHandler.getValue("jetty.port", "8050"));
+        assertEquals(propertyHandler.getValue("jetty.port", "def"), "8050");
         assertEquals(propertyHandler.getValue("not-found-key", "default-value"), "default-value");
 
-        // TODO: test props
+        assertEquals(propertyHandler.getValue("settings.filename", "default-value"), "transformerSettings.ser");
+        assertEquals(propertyHandler.getValue("doclet.filename", "default-value"), "json-doclet-1.0.7.jar");
+        assertEquals(propertyHandler.getValue("doclet.classname", "default-value"), "com.github.markusbernhardt.xmldoclet.XmlDoclet");
+        assertEquals(propertyHandler.getValue("doclet.outfilename", "default-value"), "javadoc.json");
+
     }
 }
--- transformer/src/test/java/com/networkedassets/autodoc/transformer/util/uml/PlantUMLTest.java
+++ transformer/src/test/java/com/networkedassets/autodoc/transformer/util/uml/PlantUMLTest.java
@@ -1,10 +1,40 @@
 package com.networkedassets.autodoc.transformer.util.uml;
 
+import net.sourceforge.plantumldependency.commoncli.exception.CommandLineException;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class PlantUMLTest {
-    @Test
-    public void testFromDirectory() {
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class PlantUMLTest{
+
+    @Test
+    public void testFromDirectory(){
+        Path path = Paths.get(System.getProperty("user.dir"));
+        Path result = null;
+        try{
+            result = PlantUML.fromDirectory(path,null,null);
+        }  catch(PlantUMLException p){
+            Assert.fail("PlantUMLException: " + p.getMessage());
+        }
+
+        Assert.assertNotNull(result);
+        Assert.assertTrue(result.getFileName().toString().matches("prefix-\\d+-suffix"));
+    }
+
+    @Test(expected = CommandLineException.class)
+    public void throwsCommandLineException() throws Throwable{
+        try {
+            PlantUML.fromDirectory(Paths.get("default"), null, null);
+        } catch(Exception e){
+           throw e.getCause();
+        }
+    }
+
+    @Test(expected = PlantUMLException.class)
+    public void throwsPlantUMLException() throws Throwable{
+        Path path = Paths.get(System.getProperty("user.dir"));
+        PlantUML.fromDirectory(path, "abc", null);
     }
 }
