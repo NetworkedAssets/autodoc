5a9f45f7d96fca9d0f7b025005b9e070a3954952
Maciej Tułaza <mtulaza@networkedassets.net>
2015-12-02 14:11:17 +0100

    dependency injection tests for classes SettingsManager, DefaultDocumentationGeneratorFactory and DocumentationFromCodeGenerator created in BinderTest class; comments added

--- transformer/src/test/java/com/networkedassets/autodoc/transformer/server/BinderTest.java
+++ transformer/src/test/java/com/networkedassets/autodoc/transformer/server/BinderTest.java
@@ -1,8 +1,10 @@
 package com.networkedassets.autodoc.transformer.server;
 
+import com.networkedassets.autodoc.transformer.handleRepoPush.core.DefaultDocumentationGeneratorFactory;
+import com.networkedassets.autodoc.transformer.handleRepoPush.core.DocumentationFromCodeGenerator;
 import com.networkedassets.autodoc.transformer.handleRepoPush.infrastructure.GitCodeProvider;
 import com.networkedassets.autodoc.transformer.handleRepoPush.require.CodeProvider;
-import junit.framework.Assert;
+import com.networkedassets.autodoc.transformer.manageSettings.core.SettingsManager;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
@@ -10,19 +12,42 @@ import org.junit.Test;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.*;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
 
-import static junit.framework.Assert.*;
+import static junit.framework.Assert.assertEquals;
 
+/**
+ * Testing dependency injection
+ */
 public class BinderTest extends JerseyTest {
 
     @Test
-    public void invokeGitCodeProvider(){
+    public void invokeGitCodeProvider() {
         invoke(GitCodeProvider.class);
     }
 
-    private <T extends CodeProvider> void invoke(Class<T> codeProvider) {
+    @Test
+    public void invokeSettingsManager() {
+        invoke(SettingsManager.class);
+    }
+
+    @Test
+    public void invokeDefaultDocumentationGeneratorFactory() {
+        invoke(DefaultDocumentationGeneratorFactory.class);
+    }
+
+    @Test
+    public void invokeDocumentationFromCodeGenerator() {
+        invoke(DocumentationFromCodeGenerator.class);
+    }
+
+    /**
+     * invokes RESTful resource to obtain class name injected in Resource class and compare its equality
+     * @param codeProvider
+     */
+    private void invoke(Class codeProvider) {
         final String codeProviderName = codeProvider.getName();
         Response response = target("example/" + codeProviderName).request().get();
         assertEquals(codeProviderName, response.readEntity(String.class));
@@ -40,15 +65,40 @@ public class BinderTest extends JerseyTest {
         }
     }
 
+    /**
+     * RESTful resource using to @Inject different classes and compare class name passed in URI to injected class name
+     */
     @Path("/example")
     public static class Resource {
         @Inject
         private CodeProvider codeProvider;
+        @Inject
+        private SettingsManager settingsManager;
+        @Inject
+        private DefaultDocumentationGeneratorFactory defaultDocumentationGeneratorFactory;
+        @Inject
+        private DocumentationFromCodeGenerator documentationFromCodeGenerator;
 
         @GET
         @Path("/{serviceClass}")
-        public Response getDynamicInvokedService() {
-            return Response.ok(codeProvider.getClass().getName()).build();
+        public Response getDynamicInvokedService(@PathParam("serviceClass") String serviceClass) {
+            String injectedServiceClassName = "";
+
+            switch(serviceClass){
+                case "com.networkedassets.autodoc.transformer.handleRepoPush.infrastructure.GitCodeProvider":
+                    injectedServiceClassName = codeProvider.getClass().getName();
+                    break;
+                case "com.networkedassets.autodoc.transformer.manageSettings.core.SettingsManager":
+                    injectedServiceClassName = settingsManager.getClass().getName();
+                    break;
+                case "com.networkedassets.autodoc.transformer.handleRepoPush.core.DefaultDocumentationGeneratorFactory":
+                    injectedServiceClassName = defaultDocumentationGeneratorFactory.getClass().getName();
+                    break;
+                case "com.networkedassets.autodoc.transformer.handleRepoPush.core.DocumentationFromCodeGenerator":
+                    injectedServiceClassName = documentationFromCodeGenerator.getClass().getName();
+                    break;
+            }
+            return Response.ok(injectedServiceClassName).build();
         }
     }
 }
