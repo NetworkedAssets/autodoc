436c85a358a7d82b1d52912d8cf4d20c26f2488b
Mariusz Gilewicz <mgilewicz@networkedassets.org>
2015-12-03 13:06:04 +0100

    Added testcases to Javadoc.fromDirectory (ones requiring doclet jar are @ignored)

--- transformer/src/test/java/com/networkedassets/autodoc/transformer/util/javadoc/JavaDocTest.java
+++ transformer/src/test/java/com/networkedassets/autodoc/transformer/util/javadoc/JavaDocTest.java
@@ -1,8 +1,14 @@
 package com.networkedassets.autodoc.transformer.util.javadoc;
 
+import com.networkedassets.autodoc.transformer.util.PropertyHandler;
 import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -26,5 +32,46 @@ public class JavaDocTest {
     @Test (expected = NullPointerException.class)
     public void testSearchJavaFilesGivenNullPathThrowsNullPointerException() throws JavadocException {
         Javadoc.searchJavaFiles(null);
+    }
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Ignore
+    @Test
+    public void testFromDirectoryGivenCorrectPathReturnsPath() throws JavadocException, IOException{
+        Path result = Javadoc.fromDirectory(folder.newFolder().toPath(), null, null);
+
+        Assert.assertNotNull(result);
+        Assert.assertTrue(result.toFile().exists());
+        Assert.assertTrue(result.toFile().isDirectory());
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void testFromDirectoryGivenNullPathThrowsNullPointerException() throws JavadocException, IOException{
+        Javadoc.fromDirectory(null, null, null);
+    }
+
+    @Test
+    public void testFromDirectoryGivenIncorrectDocletPathAndDocletClassCreatesEmptyDirectory() throws JavadocException, IOException{
+        Path result = Javadoc.fromDirectory(folder.newFolder().toPath(), "abc", "def");
+
+        Assert.assertNotNull(result);
+        Assert.assertTrue(result.toFile().exists());
+        Assert.assertTrue(result.toFile().isDirectory());
+        Assert.assertTrue(result.toFile().length() == 0);
+    }
+
+    @Ignore
+    @Test
+    public void testFromDirectoryGivenCorrectDocletPathAndDocletClassCreatesDirectory() throws JavadocException, IOException{
+        Path result = Javadoc.fromDirectory(folder.newFolder().toPath(),
+                new File(PropertyHandler.getInstance().getValue("doclet.filename", null)).getAbsolutePath(),
+                PropertyHandler.getInstance().getValue("doclet.classname", null));
+
+        Assert.assertNotNull(result);
+        Assert.assertTrue(result.toFile().exists());
+        Assert.assertTrue(result.toFile().isDirectory());
+        Assert.assertTrue(result.toFile().length() != 0);
     }
 }
