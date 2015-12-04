a9c150865f9c31d9c13a17fd3116228da1e70121
PGWIZDALSKI <PGWIZDALSKI@192.168.10.110>
2015-12-04 15:57:59 +0100

    Add junit test GitCodeProviderTest

---
+++ transformer/src/test/java/com/networkedassets/autodoc/transformer/infrastructure/GitCodeProviderTest.java
@@ -0,0 +1,51 @@
+package com.networkedassets.autodoc.transformer.infrastructure;
+
+import java.io.IOException;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import com.networkedassets.autodoc.transformer.handleRepoPush.infrastructure.GitCodeProvider;
+import com.networkedassets.autodoc.transformer.settings.Source;
+
+public class GitCodeProviderTest {
+
+	// To do: add some test to clone repository
+
+	@Rule
+	public TemporaryFolder tempFolder = new TemporaryFolder();
+
+	private Git remote;
+
+	@Before
+	public void setUp() throws GitAPIException, IOException {
+		remote = Git.init().setDirectory(tempFolder.newFolder("remote")).call();
+		remote.commit().setMessage("Initial commit").call();
+		remote.branchCreate().setName("develop").call();
+
+	}
+
+	@After
+	public void tearDown() {
+		remote.close();
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testGetCodeGivenNoProperUrlThrowsRuntimeException() throws Exception {
+
+		Source source = new Source();
+		source.setPassword("-");
+		source.setUsername("-");
+		source.setUrl("-");
+
+		GitCodeProvider gitProvider = new GitCodeProvider();
+		gitProvider.getCode(source, "", "", "");
+
+	}
+
+}
