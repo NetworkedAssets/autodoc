043ddfecbd74a90468f7a4f09d5621cebdb8be45
PGWIZDALSKI <PGWIZDALSKI@192.168.10.110>
2015-12-11 09:41:13 +0100

    Remove unused test and remove unnecessary properties

--- transformer/src/test/java/com/networkedassets/autodoc/transformer/util/PropertyHandlerTest.java
+++ transformer/src/test/java/com/networkedassets/autodoc/transformer/util/PropertyHandlerTest.java
@@ -16,6 +16,7 @@ public class PropertyHandlerTest {
 		assertNotNull(propertyHandler.getValue("jetty.port", "8050"));
 		assertEquals(propertyHandler.getValue("jetty.port", "def"), "8050");
 	}
+
 	@Test
 	public void testGetValueNotFoundKey() throws IOException {
 		PropertyHandler propertyHandler = PropertyHandler.getInstance();
@@ -24,6 +25,7 @@ public class PropertyHandlerTest {
 		assertNotNull(propertyHandler.getValue("not-found-key", "default-value"));
 		assertEquals(propertyHandler.getValue("not-found-key", "default-value"), "default-value");
 	}
+
 	@Test
 	public void testGetValueSeetingsFilename() throws IOException {
 		PropertyHandler propertyHandler = PropertyHandler.getInstance();
@@ -32,29 +34,5 @@ public class PropertyHandlerTest {
 		assertNotNull(propertyHandler.getValue("settings.filename", "transformerSettings.ser"));
 		assertEquals(propertyHandler.getValue("settings.filename", "default-value"), "transformerSettings.ser");
 	}
-	@Test
-	public void testGetValueDocletFilename() throws IOException {
-		PropertyHandler propertyHandler = PropertyHandler.getInstance();
-		assertNotNull(propertyHandler);
 
-		assertNotNull(propertyHandler.getValue("doclet.filename", "json-doclet-1.0.7.jar"));
-		assertEquals(propertyHandler.getValue("doclet.filename", "default-value"), "json-doclet-1.0.7.jar");
-		}
-	@Test
-	public void testGetValueDocletClassname() throws IOException {
-		PropertyHandler propertyHandler = PropertyHandler.getInstance();
-		assertNotNull(propertyHandler);
-
-		assertNotNull(propertyHandler.getValue("doclet.classname", "com.github.markusbernhardt.xmldoclet.XmlDoclet"));
-		assertEquals(propertyHandler.getValue("doclet.classname", "default-value"),
-				"com.github.markusbernhardt.xmldoclet.XmlDoclet");
-	}
-	@Test
-	public void testGetValueDocletOutFilename() throws IOException {
-		PropertyHandler propertyHandler = PropertyHandler.getInstance();
-		assertNotNull(propertyHandler);
-
-		assertNotNull(propertyHandler.getValue("doclet.outfilename", "javadoc.json"));
-		assertEquals(propertyHandler.getValue("doclet.outfilename", "default-value"), "javadoc.json");
-	}
 }
