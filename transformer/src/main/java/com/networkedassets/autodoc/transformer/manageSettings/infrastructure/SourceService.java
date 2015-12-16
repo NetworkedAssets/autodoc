10b9d9ae30af9a2038bcf648f86df174a5fc6544
kcala <kcala@networkedassets.org>
2015-12-16 17:51:26 +0100

    Add sources get endpoint

--- transformer/src/main/java/com/networkedassets/autodoc/transformer/manageSettings/infrastructure/SourceService.java
+++ transformer/src/main/java/com/networkedassets/autodoc/transformer/manageSettings/infrastructure/SourceService.java
@@ -1,21 +1,30 @@
 package com.networkedassets.autodoc.transformer.manageSettings.infrastructure;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import com.networkedassets.autodoc.transformer.manageSettings.provide.out.SettingsProvider;
 import com.networkedassets.autodoc.transformer.settings.Source;
+import com.networkedassets.autodoc.transformer.settings.SourceCheckingDeserializer;
+import com.networkedassets.autodoc.transformer.settings.SourceCheckingSerializer;
+import com.networkedassets.autodoc.transformer.util.RestErrorMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Optional;
 
 /**
  * REST service intended to check whether given source exists
  */
 
-@Path("/source")
+@Path("/sources/")
 public class SourceService {
 
     private SettingsProvider settingsProvider;
@@ -27,12 +36,27 @@ public class SourceService {
 
     static final Logger log = LoggerFactory.getLogger(SourceService.class);
 
-    // TODO: 16.12.2015 IMPLEMENT
     @GET
+    @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Source getSource() {
+    public Response getSource(@PathParam("id") int sourceId) {
         log.info("GET request for source handled");
-        settingsProvider.getCurrentSettings().getSources();
-        return null;
+        Optional<Source> requestedSource = settingsProvider.getCurrentSettings().getSources().stream()
+                .filter(source -> source.getId()==sourceId)
+                .findFirst();
+
+        if (!requestedSource.isPresent()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        } else {
+            return Response.status(Response.Status.OK)
+                    .type(MediaType.APPLICATION_JSON)
+                    .entity(requestedSource.orElse(null))
+                    .build();
+        }
+
     }
+
+
+
+
 }
--- transformer/src/main/java/com/networkedassets/autodoc/transformer/util/RestErrorMessage.java
+++ transformer/src/main/java/com/networkedassets/autodoc/transformer/util/RestErrorMessage.java
@@ -4,27 +4,15 @@ package com.networkedassets.autodoc.transformer.util;
  * Created by kamil on 26.11.2015.
  */
 public class RestErrorMessage {
-    private int status;
     private String message;
-    private String link;
     private String developerMessage;
 
     public RestErrorMessage() {
     }
 
-    public RestErrorMessage(int status, String message, String link, String developerMessage) {
-        this.status = status;
+    public RestErrorMessage(String message, String developerMessage) {
         this.message = message;
-        this.link = link;
         this.developerMessage = developerMessage;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
     }
 
     public String getMessage() {
@@ -33,14 +21,6 @@ public class RestErrorMessage {
 
     public void setMessage(String message) {
         this.message = message;
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
     }
 
     public String getDeveloperMessage() {
