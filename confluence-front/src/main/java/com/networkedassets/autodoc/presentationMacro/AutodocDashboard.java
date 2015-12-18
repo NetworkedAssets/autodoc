500b7437a854d3db5a1f516b2a141619a0758c62
Mikołaj Robakowski <mrobakowski@networkedassets.org>
2015-12-18 10:34:19 +0100

    Change the presentation macro to use the new macro infrastructure

--- confluence-front/src/main/java/com/networkedassets/autodoc/presentationMacro/AutodocDashboard.java
+++ confluence-front/src/main/java/com/networkedassets/autodoc/presentationMacro/AutodocDashboard.java
@@ -1,39 +1,27 @@
 package com.networkedassets.autodoc.presentationMacro;
 
+import com.atlassian.confluence.content.render.xhtml.ConversionContext;
+import com.atlassian.confluence.macro.Macro;
+import com.atlassian.confluence.macro.MacroExecutionException;
 import com.atlassian.confluence.renderer.radeox.macros.MacroUtils;
 import com.atlassian.confluence.util.velocity.VelocityUtils;
-import com.atlassian.renderer.RenderContext;
-import com.atlassian.renderer.v2.RenderMode;
-import com.atlassian.renderer.v2.macro.BaseMacro;
-import com.atlassian.renderer.v2.macro.MacroException;
 import org.apache.commons.io.IOUtils;
 import org.apache.velocity.VelocityContext;
 
 import java.io.IOException;
 import java.util.Map;
 
-public class AutodocDashboard extends BaseMacro {
-
-
-    @Override
-    public boolean hasBody() {
-        return false;
-    }
+public class AutodocDashboard implements Macro {
 
     @Override
-    public RenderMode getBodyRenderMode() {
-        return RenderMode.NO_RENDER;
-    }
-
-    @Override
-    public String execute(Map map, String s, RenderContext renderContext) throws MacroException {
+    public String execute(Map<String, String> params, String s, ConversionContext conversionContext) throws MacroExecutionException {
         String dashboardSection;
         String resourcesPath = "download/resources/com.networkedassets.autodoc.confluence-front:dashboard-resources/dashboardResources/";
 
         try {
             dashboardSection = IOUtils.toString(this.getClass().getClassLoader().getResourceAsStream("/dashboardResources/index.html"));
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new MacroExecutionException(e);
         }
 
         dashboardSection = ("<section" + dashboardSection.split("<section")[1]).split("section>")[0] + "section>";
@@ -41,7 +29,20 @@ public class AutodocDashboard extends BaseMacro {
         VelocityContext context = new VelocityContext(MacroUtils.defaultVelocityContext());
         context.put("dashboardSectionHtml", dashboardSection);
         context.put("resourcesPath", resourcesPath);
+        context.put("project", params.get("project"));
+        context.put("repo", params.get("repo"));
+        context.put("branch", params.get("branch"));
 
         return VelocityUtils.getRenderedTemplate("/dashboardResources/dashboard.vm", context);
+    }
+
+    @Override
+    public BodyType getBodyType() {
+        return BodyType.NONE;
+    }
+
+    @Override
+    public OutputType getOutputType() {
+        return OutputType.BLOCK;
     }
 }
---
+++ confluence-front/src/main/resources/js/autodocMacroParams.js
@@ -0,0 +1,3 @@
+/**
+ * Created by mrobakowski on 12/17/2015.
+ */
