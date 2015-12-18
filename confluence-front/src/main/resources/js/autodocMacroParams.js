bdb9e3e78cc12fad45c77b26e4acfe9758882d8d
Mikołaj Robakowski <mrobakowski@networkedassets.org>
2015-12-18 15:19:49 +0100

    Start implementing macro settings dialog

--- confluence-front/src/main/resources/js/autodocMacroParams.js
+++ confluence-front/src/main/resources/js/autodocMacroParams.js
@@ -1,3 +1,47 @@
 /**
  * Created by mrobakowski on 12/17/2015.
  */
+(function ($) {
+    var jsOverrides = {
+        "fields": {
+            "enum": function (params, options) {
+                if (params && params.name == "project") {
+                    var paramDiv = $(Confluence.Templates.MacroBrowser.macroParameterSelect());
+                    var $select = $("select", paramDiv);
+                    // TODO: fetch all available projects and put them here
+                    $(["a, b, c", "d, e, f"]).each(function () {
+                        $select.append($("<option/>").attr("value", this).html("" + this));
+                    });
+
+                    var field = AJS.MacroBrowser.Field(paramDiv, $select, options);
+                    return field;
+                } else if (params && params.name == "repo") {
+                    var paramDiv = $(Confluence.Templates.MacroBrowser.macroParameterSelect());
+                    var $select = $("select", paramDiv);
+                    // TODO: fetch repos and put them here
+                    // TODO: should take chosen project into consideration
+                    $(["a, b, c", "d, e, f"]).each(function () {
+                        $select.append($("<option/>").attr("value", this).html("" + this));
+                    });
+
+                    var field = AJS.MacroBrowser.Field(paramDiv, $select, options);
+                    return field;
+                } else if (params && params.name == "project") {
+                    var paramDiv = $(Confluence.Templates.MacroBrowser.macroParameterSelect());
+                    var $select = $("select", paramDiv);
+                    // TODO: fetch branches and put them here
+                    // TODO: should take chosen repo into consideration
+                    $(["a, b, c", "d, e, f"]).each(function () {
+                        $select.append($("<option/>").attr("value", this).html("" + this));
+                    });
+
+                    var field = AJS.MacroBrowser.Field(paramDiv, $select, options);
+                    return field;
+                }
+
+                return AJS.MacroBrowser.ParameterFields["enum"](params, options);
+            }
+        }
+    };
+    AJS.MacroBrowser.setMacroJsOverride("autodocDashboard", jsOverrides);
+})(AJS.$);
