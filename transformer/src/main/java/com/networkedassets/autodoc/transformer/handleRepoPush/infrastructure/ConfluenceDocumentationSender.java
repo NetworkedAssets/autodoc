ddc0ad2b8cd4cf6b6fedab436dfd83a097dbbe69
Mikołaj Robakowski <mrobakowski@networkedassets.org>
2016-01-07 10:35:31 +0100

    Change the url escaper to a more safe one

--- transformer/src/main/java/com/networkedassets/autodoc/transformer/handleRepoPush/infrastructure/ConfluenceDocumentationSender.java
+++ transformer/src/main/java/com/networkedassets/autodoc/transformer/handleRepoPush/infrastructure/ConfluenceDocumentationSender.java
@@ -1,15 +1,14 @@
 package com.networkedassets.autodoc.transformer.handleRepoPush.infrastructure;
 
 import com.google.common.base.Strings;
+import com.google.common.escape.Escaper;
+import com.google.common.net.UrlEscapers;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import com.networkedassets.autodoc.transformer.handleRepoPush.Documentation;
 import com.networkedassets.autodoc.transformer.handleRepoPush.DocumentationPiece;
 import com.networkedassets.autodoc.transformer.handleRepoPush.require.DocumentationSender;
 import com.networkedassets.autodoc.transformer.settings.Settings;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 
 public class ConfluenceDocumentationSender implements DocumentationSender {
     private static final String confluenceEndpointFormat = "%s/rest/autodoc/1.0/documentation/%s/%s/%s/%s/%s";
@@ -21,14 +20,14 @@ public class ConfluenceDocumentationSender implements DocumentationSender {
         url = url.endsWith("/") ? url.substring(0, url.length() - 1) : url;
         for (DocumentationPiece docPiece : documentation.getPieces()) {
             try {
-                // TODO: fix the bug where for some reason, even though it is escaped, branches in the form of refs/heads/master do not work
+                Escaper e = UrlEscapers.urlPathSegmentEscaper();
                 System.out.println(Unirest.post(
                         String.format(confluenceEndpointFormat, url,
-                                URLEncoder.encode(documentation.getProject(), "UTF-8"),
-                                URLEncoder.encode(documentation.getRepo(), "UTF-8"),
-                                URLEncoder.encode(documentation.getBranch(), "UTF-8"),
-                                URLEncoder.encode(documentation.getType().toString(), "UTF-8"),
-                                URLEncoder.encode(docPiece.getPieceName(), "UTF-8")))
+                                e.escape(documentation.getProject()),
+                                e.escape(documentation.getRepo()),
+                                e.escape(documentation.getBranch()),
+                                e.escape(documentation.getType().toString()),
+                                e.escape(docPiece.getPieceName())))
                         .basicAuth(settings.getConfluenceUsername(), settings.getConfluencePassword())
                         .queryString("pieceType", docPiece.getPieceType())
                         .header("Content-Type", "application/json")
@@ -36,8 +35,6 @@ public class ConfluenceDocumentationSender implements DocumentationSender {
                         .asString().getBody());
             } catch (UnirestException e) {
                 throw new RuntimeException(e);
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
             }
         }
     }
