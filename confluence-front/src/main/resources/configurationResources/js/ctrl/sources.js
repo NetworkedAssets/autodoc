a56cad3aab48e651cf1535c5330021abaa77eafb
Jakub Wojnowski <jwojnowski@networkedassets.org>
2016-01-22 17:28:34 +0100

    Config GUI: preparations for App Links-driven sources

--- confluence-front/src/main/resources/configurationResources/js/ctrl/sources.js
+++ confluence-front/src/main/resources/configurationResources/js/ctrl/sources.js
@@ -1,150 +1,62 @@
-angular.module("DoC_Config").controller("sourcesCtrl",function($scope,$http,settingsData,$timeout,urlProvider) {
-    var sources = this;
+angular.module("DoC_Config").controller("sourcesCtrl",function($http,urlProvider) {
+    var vm = this;
 
-    var url = urlProvider.getRestUrl("/source");
+    vm.sources = [];
 
-
-    var extractSourceDataForRest = function(original) {
-        var data = {};
-
-        data.name = original.name;
-        data.url = original.url;
-        data.sourceType = original.sourceType;
-        data.password = original.password;
-        data.username = original.username;
-        return data;
-    };
-
-    sources.list = [];
-
-    sources.availableKinds = [
-        {
-            value: "BITBUCKET",
-            label: "Bitbucket"
-        },
-        {
-            value: "STASH",
-            label: "Stash"
-        }
-    ];
-
-    $scope.$on("settingsData.ready",function() {
-        sources.list = [];
-        if (settingsData.get().sources) {
-
-        }
-        angular.forEach(settingsData.get().sources,function(source,key) {
-            sources.add({
-                id: source.id,
-                name: source.name,
-                url: source.url,
-                sourceType: source.sourceType,
-                username: source.username,
-                password: null,
-                verified: true,
-                nameCorrect: true,
-                sourceExists: true,
-                credentialsCorrect: true,
-                sourceTypeCorrect: true,
-                dirty: false
-            });
-        });
-    });
-
-    sources.removeFromList = function(index) {
-        sources.list.splice(index,1);
-        $timeout();
-    };
-
-    sources.changed = function(source) {
-        source.verified = false;
-        source.dirty = true;
-    };
-
-    sources.add = function(source) {
-
-        if (!source) {
-            source = {
-                "id": null,
-                "name": "",
-                "url": "",
-                "sourceType": "STASH",
-                "username": "",
-                "password": "",
-                "hookKey": "",
-                "verified": false,
-                dirty: true
-            };
-        }
-
-        source.initializing = true;
-
-        sources.list.push(source);
-
-        $scope.$watch(function() {
-            return source;
-        },function(newValue,oldValue) {
-            if (newValue!==oldValue) {
-                sources.changed(source);
+    var load = function() {
+        var success = function(response) {
+            vm.sources = response.data.sources;
+        };
+        var error = function(response) {
+            var sources = [];
+        };
+        setTimeout(function() {
+            var response = {
+                data: {
+                    "sources": [
+                        {
+                            "id": 0,
+                            "credentialsCorrect": false,
+                            "hookKey": "com.networkedassets.atlasian.plugins.stash-postReceive-hook-plugin:postReceiveHookListener",
+                            "projects": {
+                            },
+                            "sourceType": "STASH",
+                            "url": "http://atlas.networkedassets.net:7990",
+                            "nameCorrect": true,
+                            "password": null,
+                            "correct": false,
+                            "username": null,
+                            "sourceTypeCorrect": false,
+                            "sourceExists": true,
+                            "name": "AtlasDev Stash"
+                        },
+                        {
+                            "id": 0,
+                            "credentialsCorrect": false,
+                            "hookKey": "com.networkedassets.atlassian.plugins.bitbucket-postReceive-hook-plugin:postReceiveHookListener",
+                            "projects": {
+                            },
+                            "sourceType": "BITBUCKET",
+                            "url": "http://atlas.networkedassets.net:7991",
+                            "nameCorrect": true,
+                            "password": null,
+                            "correct": false,
+                            "username": null,
+                            "sourceTypeCorrect": false,
+                            "sourceExists": true,
+                            "name": "Bitbucket"
+                        }
+                    ]
+                },
+                status: "OK"
             }
-        },true);
+            success.call(this,response);
+        },500);
+        /*
+        $http
+            .get(urlProvider.getRestUrlWithParams(["applinks","sources"]))
+            .then(success,error);*/
     };
 
-
-
-    sources.save = function(index) {
-        var original = sources.list[index];
-        var data = extractSourceDataForRest(original);
-
-        var id = original.id;
-
-        var req;
-        if (original.id === null) {
-            req = $http.post(url,data);
-        } else {
-            req = $http.put(url+"/"+original.id,data);
-        }
-
-        req.then(function(resp) {
-            sources.list[index] = resp.data;
-            sources.list[index].id = resp.data.id;
-            sources.list[index].dirty = false;
-            sources.list[index].verified = true;
-            settingsData.reload();
-        },function(resp) {
-            if (resp.status == "400") {
-                sources.list[index] = resp.data;
-                sources.list[index].id = null;
-                sources.list[index].dirty = false;
-                sources.list[index].verified = false;
-            }
-        });
-
-    };
-
-    sources.delete = function(index) {
-        var source = sources.list[index];
-        if (source.id === null) {
-            sources.removeFromList(index);
-        } else {
-            source.deletionState = "confirming";
-        }
-    };
-
-    sources.cancelDeletion = function(index) {
-        var source = sources.list[index];
-        source.deletionState = null;
-    };
-
-    sources.deleteExecute = function(index) {
-        var source = sources.list[index];
-        source.deletionState = "executing";
-        $http.delete(url+"/"+source.id).then(function(response) {
-            sources.removeFromList(index);
-            settingsData.reload();
-        });
-
-    };
-
-
+    load();
 });
