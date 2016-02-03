f671230a58c5edcc8ec5a06b41dfd725e7c43194
kcala <kcala@networkedassets.org>
2016-02-03 15:35:55 +0100

    Unify TransformerClient methods looks, and make them more transparent - all error codes and so on from the transformed are passed through and not altered in any way anymore.
    Now client behaves 100% as a proxy, just passing and returning requests.

--- confluence-front/src/main/java/com/networkedassets/autodoc/transformer/TransformerClient.java
+++ confluence-front/src/main/java/com/networkedassets/autodoc/transformer/TransformerClient.java
@@ -24,12 +24,12 @@ import com.networkedassets.autodoc.transformer.settings.SettingsException;
 import com.networkedassets.autodoc.transformer.settings.Source;
 
 public class TransformerClient {
-    public static final String SETTINGS = "/settings";
-    public static final String CREDENTIALS = "/settings/credentials";
-    public static final String SOURCES = "/sources/";
-    public static final String EXTENDED_SOURCES = "/sources/extended/";
-    public static final String EVENT = "/event";
-    public static final String EVENT_JSON = "{\"sourceUrl\":\"%s\",\"projectKey\":\"%s\",\"repositorySlug\":\"%s\",\"branchId\":\"%s\"}";
+    private static final String SETTINGS = "/settings";
+    private static final String CREDENTIALS = "/settings/credentials";
+    private static final String SOURCES = "/sources/";
+    private static final String EXTENDED_SOURCES = "/sources/extended/";
+    private static final String EVENT = "/event";
+    private static final String EVENT_JSON = "{\"sourceUrl\":\"%s\",\"projectKey\":\"%s\",\"repositorySlug\":\"%s\",\"branchId\":\"%s\"}";
 
     public static final Logger log = LoggerFactory.getLogger(TransformerClient.class);
 
@@ -42,156 +42,107 @@ public class TransformerClient {
         Unirest.setHttpClient(getConfiguredHttpClient());
     }
 
-
     public HttpResponse<String> getSettings() throws SettingsException {
-        HttpResponse<String> response;
         try {
-
-            response = Unirest.get(url + SETTINGS).asString();
+            return Unirest.get(url + SETTINGS).asString();
         } catch (UnirestException e) {
             throw new SettingsException(e);
         }
-
-        return response;
     }
 
     public HttpResponse<String> getCredentials() throws SettingsException {
-        HttpResponse<String> response;
         try {
-            response = Unirest.get(url + CREDENTIALS).header("Content-Type", "application/json").asString();
+            Unirest.get(url).asString();
+            return Unirest.get(url + CREDENTIALS).header("Content-Type", "application/json").asString();
         } catch (UnirestException e) {
             throw new SettingsException(e);
         }
-        return response;
     }
 
     public HttpResponse<String> setCredentials(Settings settings) throws SettingsException {
-        HttpResponse<String> response;
         try {
-            response = Unirest.post(url + CREDENTIALS).header("Content-Type", "application/json").body(settings)
+            return Unirest.post(url + CREDENTIALS).header("Content-Type", "application/json").body(settings)
                     .asString();
         } catch (UnirestException e) {
             throw new SettingsException(e);
         }
-        return response;
     }
 
     public HttpResponse<String> modifyBranch(int sourceId, String projectKey, String repoSlug, String branchId, Branch branch)
             throws SettingsException {
         try {
-            HttpResponse<String> branchHttpResponse = Unirest
+            return Unirest
                     .put(url + "/sources/{sourceId}/{projectKey}/{repoSlug}/{branchId}")
                     .routeParam("sourceId", Integer.toString(sourceId))
                     .routeParam("projectKey", URLEncoder.encode(projectKey, "UTF-8"))
                     .routeParam("repoSlug", URLEncoder.encode(repoSlug, "UTF-8"))
                     .routeParam("branchId", URLEncoder.encode(branchId, "UTF-8"))
                     .header("Content-Type", "application/json").body(branch).asString();
-            if (branchHttpResponse.getStatus() == 200) {
-                return branchHttpResponse;
-            } else {
-                throw new SettingsException(
-                        "Could not modify branch: " + IOUtils.toString(branchHttpResponse.getRawBody()));
-            }
         } catch (UnirestException | IOException e) {
             throw new SettingsException(e);
         }
     }
 
     public HttpResponse<String> getSources() throws SettingsException {
-        HttpResponse<String> response;
         try {
-            response = Unirest.get(url + SOURCES).asString();
+            return Unirest.get(url + SOURCES).asString();
         } catch (UnirestException e) {
             throw new SettingsException(e);
         }
-
-        return response;
     }
 
     public HttpResponse<String> getExtendedSources() throws SettingsException {
-        HttpResponse<String> response;
         try {
-            response = Unirest.get(url + EXTENDED_SOURCES).asString();
+            return Unirest.get(url + EXTENDED_SOURCES).asString();
         } catch (UnirestException e) {
             throw new SettingsException(e);
         }
-
-        return response;
     }
-
 
     public HttpResponse<String> forceGenerate(String sourceUrl, String projectKey, String repoSlug, String branchId)
             throws SettingsException {
         String eventPayload = String.format(EVENT_JSON, sourceUrl, projectKey, repoSlug, branchId);
-        HttpResponse<String> response;
         try {
-
-            response = Unirest.post(url + EVENT).header("Content-Type", "application/json").body(eventPayload)
+            return Unirest.post(url + EVENT).header("Content-Type", "application/json").body(eventPayload)
                     .asString();
         } catch (UnirestException e) {
             throw new SettingsException(e);
         }
-
-        if (response.getStatus() != 200) {
-            throw new SettingsException("Error in transformer: " + response);
-        }
-        return response;
     }
 
     public HttpResponse<String> getSource(String id) throws SettingsException {
-        HttpResponse<String> response;
         try {
-            response = Unirest.get(url + SOURCES + "{id}").routeParam("id", id).asString();
+            return Unirest.get(url + SOURCES + "{id}").routeParam("id", id).asString();
         } catch (UnirestException e) {
             throw new SettingsException(e);
         }
-
-        return response;
-
     }
 
     public HttpResponse<Source> setSource(Source source) throws SettingsException {
-
-        HttpResponse<Source> response;
         try {
-
-            response = Unirest.post(url + SOURCES).header("Content-Type", "application/json").body(source)
+            return Unirest.post(url + SOURCES).header("Content-Type", "application/json").body(source)
                     .asObject(Source.class);
         } catch (UnirestException e) {
             throw new SettingsException(e);
         }
-
-        return response;
-
     }
 
     public HttpResponse<Source> changeSource(int sourceId, Source source) throws SettingsException {
-
-        HttpResponse<Source> response;
         try {
 
-            response = Unirest.put(url + SOURCES + String.valueOf(sourceId)).header("Content-Type", "application/json")
+            return Unirest.put(url + SOURCES + String.valueOf(sourceId)).header("Content-Type", "application/json")
                     .body(source).asObject(Source.class);
         } catch (UnirestException e) {
             throw new SettingsException(e);
         }
-
-        return response;
-
     }
 
     public HttpResponse<String> removeSource(int sourceId) throws SettingsException {
-
-        HttpResponse<String> response;
         try {
-
-            response = Unirest.delete(url + SOURCES + String.valueOf(sourceId)).asString();
+            return Unirest.delete(url + SOURCES + String.valueOf(sourceId)).asString();
         } catch (UnirestException e) {
             throw new SettingsException(e);
         }
-
-        return response;
-
     }
 
     public String getUrl() {
