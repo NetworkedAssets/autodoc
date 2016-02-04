82677bc35bae4357d41d0e24d2e892a329301c30
Jakub Wojnowski <jwojnowski@networkedassets.org>
2016-02-04 11:50:27 +0100

    ClassDiagram moved to Javadoc, a few refactors

--- confluence-front/src/main/resources/dashboardResources/js/ctrl/ClassDiagramCtrl.js
+++ confluence-front/src/main/resources/dashboardResources/js/ctrl/ClassDiagramCtrl.js
@@ -1,20 +1,3 @@
 angular.module("DoC").controller('ClassDiagramCtrl',function($scope,$http,$element,$timeout,urlService) {
-    $scope.loading = true;
-    var cd = new ClassDiagram({
-        elem: $("#doc_classDiagram_paper")
-    });
 
-    var initSpinner = function() {
-        AJS.$($element.find(".loadingSpinner")).spin("big");
-    };
-
-    initSpinner();
-
-    $http.get(urlService.getRestUrl("UML","all"),{
-        cache: true
-    })
-        .then(function(response) {
-            cd.generate(response.data);
-            $scope.loading = false;
-        });
 });
