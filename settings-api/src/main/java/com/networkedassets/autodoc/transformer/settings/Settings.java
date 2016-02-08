ba683c03a3eea2423742809d6b74ef4ba187c4f1
p.gwizdalski <p.gwizdalski@networkedassets.org>
2016-02-08 15:13:29 +0100

    Fix return null password for get settings by rest

--- settings-api/src/main/java/com/networkedassets/autodoc/transformer/settings/Settings.java
+++ settings-api/src/main/java/com/networkedassets/autodoc/transformer/settings/Settings.java
@@ -4,8 +4,10 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.networkedassets.autodoc.transformer.settings.view.Views;
 
@@ -15,78 +17,82 @@ import com.networkedassets.autodoc.transformer.settings.view.Views;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Settings implements Serializable {
 
-    private static final long serialVersionUID = 3847560203140549969L;
-    @JsonView(Views.GetSettingsView.class)
-    private String confluenceUrl;
-    @JsonView(Views.GetSettingsView.class)
-    private TransformerSettings transformerSettings = new TransformerSettings();
-    @JsonView(Views.GetSourcesView.class)
-    private List<Source> sources = new ArrayList<>();
-    @JsonView(Views.GetCredentialsView.class)
-    private Credentials credentials = new Credentials();
+	private static final long serialVersionUID = 3847560203140549969L;
+	@JsonView(Views.GetSettingsView.class)
+	private String confluenceUrl;
+	@JsonView(Views.GetSettingsView.class)
+	private TransformerSettings transformerSettings = new TransformerSettings();
+	@JsonView(Views.GetSourcesView.class)
+	private List<Source> sources = new ArrayList<>();
+	@JsonView(Views.GetCredentialsView.class)
+	private Credentials credentials = new Credentials();
 
-    public Source getSourceByUrl(String url) {
-        return sources.stream().filter(source -> source.getUrl().equals(url)).findAny().orElse(null);
-    }
+	public Source getSourceByUrl(String url) {
+		return sources.stream().filter(source -> source.getUrl().equals(url)).findAny().orElse(null);
+	}
 
-    public Source getSourceById(int id) {
-        return sources.stream().filter(source -> source.getId() == id).findAny().orElse(null);
-    }
+	public Source getSourceById(int id) {
+		return sources.stream().filter(source -> source.getId() == id).findAny().orElse(null);
+	}
 
-    public boolean isSourceWithUrlExistent(String url) {
-        return sources.stream().anyMatch(source -> source.getUrl().equals(url));
-    }
+	public boolean isSourceWithUrlExistent(String url) {
+		return sources.stream().anyMatch(source -> source.getUrl().equals(url));
+	}
 
-    @JsonProperty("transformerSettings")
-    public TransformerSettings getTransformerSettings() {
-        return transformerSettings;
-    }
+	@JsonProperty("transformerSettings")
+	public TransformerSettings getTransformerSettings() {
+		return transformerSettings;
+	}
 
-    public void setTransformerSettings(TransformerSettings transformerSettings) {
-        this.transformerSettings = transformerSettings;
-    }
+	public void setTransformerSettings(TransformerSettings transformerSettings) {
+		this.transformerSettings = transformerSettings;
+	}
 
-    @JsonProperty("sources")
-    public List<Source> getSources() {
-        return sources;
-    }
+	@JsonProperty("sources")
+	public List<Source> getSources() {
+		return sources;
+	}
 
-    public void setSources(List<Source> sources) {
-        this.sources = sources;
-    }
+	public void setSources(List<Source> sources) {
+		this.sources = sources;
+	}
 
-    @JsonProperty("confluenceUrl")
-    public String getConfluenceUrl() {
-        return confluenceUrl;
-    }
+	@JsonProperty("confluenceUrl")
+	public String getConfluenceUrl() {
+		return confluenceUrl;
+	}
 
-    public void setConfluenceUrl(String confluenceUrl) {
-        this.confluenceUrl = confluenceUrl;
-    }
+	public void setConfluenceUrl(String confluenceUrl) {
+		this.confluenceUrl = confluenceUrl;
+	}
 
-    @JsonProperty("confluenceUsername")
-    public String getConfluenceUsername() {
-        return credentials.getConfluenceUsername();
-    }
+	public String getConfluenceUsername() {
+		return credentials.getConfluenceUsername();
+	}
 
-    public void setConfluenceUsername(String confluenceUsername) {
-        this.credentials.setConfluenceUsername(confluenceUsername);
-    }
+	public void setConfluenceUsername(String confluenceUsername) {
+		this.credentials.setConfluenceUsername(confluenceUsername);
+	}
 
-    @JsonProperty("confluencePassword")
-    public String getConfluencePassword() {
-        return credentials.getConfluencePassword();
-    }
+	@JsonGetter("confluencePassword")
+	public String getNullConfluencePassword() {
+		return null;
+	}
 
-    public void setConfluencePassword(String confluencePassword) {
-        credentials.setConfluencePassword(confluencePassword);
-    }
+	public String getConfluencePassword() {
+		return credentials.getConfluencePassword();
+	}
 
-    public Credentials getCredentials() {
-        return credentials;
-    }
+	@JsonSetter("confluencePassword")
+	public void setConfluencePassword(String confluencePassword) {
+		credentials.setConfluencePassword(confluencePassword);
+	}
 
-    public void setCredentials(Credentials credentials) {
-        this.credentials = credentials;
-    }
+	public Credentials getCredentials() {
+		return credentials;
+	}
+
+	public void setCredentials(Credentials credentials) {
+		this.credentials = credentials;
+	}
 }
